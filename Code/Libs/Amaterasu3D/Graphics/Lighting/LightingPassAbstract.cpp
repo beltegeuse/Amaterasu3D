1de321fd3e139195f41e80a3a65bbfb5a1f5aaf8
beltegeuse <adrien.gruson@gmail.com>
2011-03-14 11:42:07 -0400

    implement abstract lighting system.

--- Code/Libs/Amaterasu3D/Graphics/Lighting/LightingPassAbstract.cpp
+++ Code/Libs/Amaterasu3D/Graphics/Lighting/LightingPassAbstract.cpp
@@ -1,17 +1,17 @@
-/*
- * LightingPassAbstract.cpp
- *
- *  Created on: Mar 13, 2011
- *      Author: Beltegeuse
- */
-
 #include "LightingPassAbstract.h"
 
 LightingPassAbstract::LightingPassAbstract() {
-	// TODO Auto-generated constructor stub
-
 }
 
 LightingPassAbstract::~LightingPassAbstract() {
-	// TODO Auto-generated destructor stub
+}
+
+void LightingPassAbstract::SetFBOGraphicBuffer(FBO* fbo)
+{
+	m_FBO_graphics = fbo;
+}
+
+void LightingPassAbstract::AddPointLight(const PointLight& light)
+{
+	m_points_light.add(light);
 }
