9d71d14a8ae7d3f104edfa8c833cdccdfa0e375d
beltegeuse <adrien.gruson@gmail.com>
2011-03-14 18:37:55 -0400

    changes debug

--- Code/Libs/Amaterasu3D/Graphics/MatrixManagement.cpp
+++ Code/Libs/Amaterasu3D/Graphics/MatrixManagement.cpp
@@ -22,13 +22,21 @@ void MatrixManagement::PushMatrix(const Math::CMatrix4& matrix)
 	if(m_maxMatrix < m_matrix.size())
 		throw CException("Matrix stack is full.");
 
+	//TODO: deprecedied functions
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+	glMultMatrixf((const float*)matrix);
+
 	// stack the new matrix
 	if(m_matrix.empty())
 		m_matrix.push_back(matrix);
 	else
 		m_matrix.push_back(matrix*m_matrix.back());
 
-	m_signal_event.emit(MODELVIEW_MATRIX, GetMatrix());
+	// TODO: to debug
+	Math::CMatrix4 matrixOpenGL;
+	matrixOpenGL.LoadFromOpenGL(GL_MODELVIEW_MATRIX);
+	m_signal_event.emit(MODELVIEW_MATRIX, matrixOpenGL);
 
 }
 
@@ -37,7 +45,12 @@ void MatrixManagement::PopMatrix()
 	if(m_matrix.empty())
 		throw CException("Matrix stack is empty. Unable to pop matrix");
 	m_matrix.pop_back();
-	m_signal_event.emit(MODELVIEW_MATRIX, GetMatrix());
+	glMatrixMode(GL_MODELVIEW);
+	glPopMatrix();
+	// TODO: to debug
+	Math::CMatrix4 matrixOpenGL;
+	matrixOpenGL.LoadFromOpenGL(GL_MODELVIEW_MATRIX);
+	m_signal_event.emit(MODELVIEW_MATRIX, matrixOpenGL);
 }
 
 bool MatrixManagement::IsEmpty() const
