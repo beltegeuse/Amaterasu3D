89494bc684fdfe756bf6abe453222f586bb570de
beltegeuse <adrien.gruson@gmail.com>
2011-03-27 18:39:17 -0400

    start logo implementation

--- Code/Libs/Amaterasu3D/Addons/Logo/Logo.cpp
+++ Code/Libs/Amaterasu3D/Addons/Logo/Logo.cpp
@@ -2,8 +2,18 @@
 
 Logo::Logo()
 {
+	m_Logo = Texture::LoadFromFile("logo.png");
 }
 
 Logo::~Logo()
 {
+}
+
+void Logo::Draw()
+{
+	m_Logo->activateTextureMapping();
+	m_Logo->activateTexture();
+
+
+	m_Logo->desactivateTextureMapping();
 }
--- Code/Libs/Amaterasu3D/Addons/Logo/Logo.h
+++ Code/Libs/Amaterasu3D/Addons/Logo/Logo.h
@@ -1,10 +1,23 @@
 #ifndef LOGO_H_
 #define LOGO_H_
 
+#include <Graphics/Texture.h>
+
 class Logo {
+private:
+	/*
+	 * Attributes
+	 */
+	TTexturePtr m_Logo; ///< Represent the Amaterasu3d logo
+
 public:
+	/*
+	 * Constructors and destructors
+	 */
 	Logo();
 	virtual ~Logo();
+
+	virtual void Draw();
 };
 
 #endif /* LOGO_H_ */
