3d8bafba43e5c10b65fa89da41b1fa5b22c7dc93
beltegeuse <adrien.gruson@gmail.com>
2011-03-27 18:26:36 -0400

    add matrix4 tests.

---
+++ Code/Tests/Amaterasu3D/Math/test_matrix4.cpp
@@ -0,0 +1,49 @@
+#include <boost/test/unit_test.hpp>
+#include <Math/Matrix4.h>
+#include <test_helper.h>
+#include <iostream>
+
+BOOST_AUTO_TEST_CASE(mat4_inv_identity)
+{
+	Math::CMatrix4 matrix;
+	matrix.Identity();
+
+	BOOST_CHECK_EQUAL(matrix,matrix.Inverse());
+}
+
+BOOST_AUTO_TEST_CASE(mat4_inv1)
+{
+	Math::CMatrix4 matrixIdent;
+	matrixIdent.Identity();
+
+	Math::CMatrix4 matrix = Math::CMatrix4::CreatePerspectiveFOV(70,1.3333,1.0,40.0);
+
+	BOOST_CHECK_EQUAL(matrixIdent,matrix*matrix.Inverse());
+}
+
+BOOST_AUTO_TEST_CASE(mat4_inv2)
+{
+	Math::CMatrix4 matrixIdent;
+	matrixIdent.Identity();
+
+	Math::CMatrix4 matrix;
+	matrix.LookAt(Math::TVector3F(10,2,5), Math::TVector3F(0,1,0));
+
+	BOOST_CHECK_EQUAL(matrixIdent,matrix*matrix.Inverse());
+}
+
+BOOST_AUTO_TEST_CASE(mat4_lookat)
+{
+	Math::CMatrix4 matrix;
+	matrix.LookAt(Math::TVector3F(0,0,0), Math::TVector3F(0,1,0));
+
+	Math::CMatrix4 matrix1;
+	matrix.LookAt(Math::TVector3F(0,0,0), Math::TVector3F(0,-1,0));
+
+	Math::CMatrix4 matrixRotation;
+	matrixRotation.SetRotationY(M_PI/2.0);
+
+	BOOST_CHECK_EQUAL(matrix,matrixRotation*matrix1);
+}
+
+
