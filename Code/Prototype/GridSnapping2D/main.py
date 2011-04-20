b67cfe2bd8d1bea64dabe17a732d7a2468a7a679
beltegeuse <adrien.gruson@gmail.com>
2011-04-20 19:12:30 -0400

    Add cascaded version in the prototype.

--- Code/Prototype/GridSnapping2D/main.py
+++ Code/Prototype/GridSnapping2D/main.py
@@ -4,7 +4,7 @@ import os
 from math import *
 
 class Gird:
-    def __init__(self, screen, i = 20, j = 20):
+    def __init__(self, screen, i = 10, j = 10):
         self.screen = screen
         self.dimension = (i, j)
         self.color = pygame.Color("White")
@@ -39,25 +39,13 @@ class Camera:
         
 
 class Volume:
-    def __init__(self, camera, i = 20, j = 20, ):
+    def __init__(self, camera, cellsize ):
         self.camera = camera
-        self.cellsize = 20
+        self.cellsize = cellsize
         self.nbCell = 16
         self.color = pygame.Color("Red")
     
     def offsetAngle(self):
-        angleNormalisation = sqrt(2)
-        sift = (self.nbCell/4)*self.cellsize
-        if(self.camera.angle >= ((7.0*pi) / 4.0) or self.camera.angle <= (pi / 4.0)):
-            self.position = (self.position[0], self.position[1] + sift*(sin(self.camera.angle))*angleNormalisation)
-        elif(self.camera.angle > (pi / 4.0) and self.camera.angle <= ((3.0*pi) / 4.0)):
-            self.position = (self.position[0] + sift*((cos(self.camera.angle)*angleNormalisation) - 1), self.position[1]+sift)
-        elif(self.camera.angle > ((3.0*pi) / 4.0) and self.camera.angle <= ((5.0*pi) / 4.0)):
-            self.position = (self.position[0]-sift*2, self.position[1] + sift*(sin(self.camera.angle))*angleNormalisation)
-        else:
-            self.position = (self.position[0] + (self.nbCell/4)*self.cellsize*((cos(self.camera.angle)*angleNormalisation) - 1), self.position[1]-(self.nbCell/4)*self.cellsize)
-    
-    def offsetAngle2(self):
         #invert vector
         x = -cos(self.camera.angle)
         y = -sin(self.camera.angle)
@@ -72,7 +60,7 @@ class Volume:
     def updatePosition(self):
         # Border offset
         self.position = (self.camera.position[0] - (self.nbCell/4)*self.cellsize, self.camera.position[1] - (self.nbCell/2)*self.cellsize)
-        self.offsetAngle2()
+        self.offsetAngle()
         # Snapping
         self.position = (floor(self.position[0] / self.cellsize) *self.cellsize , floor(self.position[1] / self.cellsize) *self.cellsize)
     
@@ -82,13 +70,24 @@ class Volume:
         pygame.draw.line(screen,color,(pos2[0], pos[1]),pos2, 2)
         pygame.draw.line(screen,color,(pos[0], pos2[1]),pos2, 2)
     
+    def drawVolume(self, position, cellsize):
+        factor = cellsize*self.nbCell
+        offset = (position[0]+factor,position[1]+factor)
+        self.drawRect(position, offset, self.color)
+        innerPos = (position[0] + factor/4, position[1] + factor/4)
+        innerPos2 = (offset[0] - factor/4, offset[1] - factor/4)
+        self.drawRect(innerPos, innerPos2, (0,255,0))
+    
+    def drawLevels(self, nbLevel, currentLevel):
+        x = self.position[0] + (self.camera.position[0] - self.position[0])*(currentLevel / (nbLevel-1))
+        y = self.position[1] + (self.camera.position[1] - self.position[1])*(currentLevel / (nbLevel-1)) 
+        self.drawVolume((x,y), self.cellsize/pow(2, currentLevel))
+    
     def draw(self, screen):
         self.updatePosition()
-        offset = (self.position[0]+self.cellsize*self.nbCell,self.position[1]+self.cellsize*self.nbCell)
-        self.drawRect(self.position, offset, self.color)
-        innerPos = (self.position[0] + (self.nbCell/4)*self.cellsize, self.position[1] + (self.nbCell/4)*self.cellsize)
-        innerPos2 = (offset[0] - (self.nbCell/4)*self.cellsize, offset[1] - (self.nbCell/4)*self.cellsize)
-        self.drawRect(innerPos, innerPos2, (0,255,0))
+        
+        self.drawVolume(self.position, self.cellsize)
+        #self.drawLevels(3.0,2)
 
 if __name__ == '__main__':
     os.environ['SDL_VIDEO_CENTERED'] = '1' 
@@ -96,7 +95,9 @@ if __name__ == '__main__':
     screen  = pygame.display.set_mode((800, 600))
     grille = Gird(screen)
     cam = Camera()
-    vol = Volume(cam)
+    vol1 = Volume(cam,20)
+    vol2 = Volume(cam,10)
+    vol3 = Volume(cam,5)
     vitesse = 2
     
     # set up key repeating so we can hold down the key to scroll.
@@ -125,7 +126,9 @@ if __name__ == '__main__':
         
         grille.draw()
         cam.draw(screen)
-        vol.draw(screen)
+        vol1.draw(screen)
+        vol2.draw(screen)
+        vol3.draw(screen)
         pygame.display.flip()
         pygame.time.wait(10)
     pygame.key.set_repeat(old_k_delay, old_k_interval)
