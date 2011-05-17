0dbeb17a63eb7e1737ab4cb99b8799003f2d9409
beltegeuse <adrien.gruson@gmail.com>
2011-05-17 19:18:19 -0400

    Add example of White balancing.

---
+++ Code/Tests/Amaterasu3DWhiteBalancing/main.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <stdlib.h>
+
+#include <Math/Matrix4.h>
+#include <Graphics/SceneGraph/Debug/DebugCubeLeaf.h>
+#include <Graphics/SceneGraph/Model.h>
+#include <Logger/LoggerFile.h>
+#include <Graphics/Lighting/DeferredLighting/DeferredLighting.h>
+#include <Application.h>
+#include <Graphics/Camera/CameraFPS.h>
+#include <Addons/FPS/FPS.h>
+
+class ApplicationWhite : public Application
+{
+protected:
+	CameraFPS* m_Camera;
+	TShaderPtr m_gbuffer_shader;
+	DeferredLighting* m_GI;
+	FPS m_FPS;
+	bool m_debug;
+public:
+	ApplicationWhite() :
+		m_debug(false)
+	{
+
+	}
+
+	virtual ~ApplicationWhite()
+	{
+	}
+
+	virtual void OnInitialize()
+	{
+		// Camera Setup
+		m_Camera = new CameraFPS(Math::TVector3F(3,4,2), Math::TVector3F(0,0,0));
+		m_Camera->SetSpeed(200.0);
+		// Initialise OpenGL
+		GLCheck(glClearColor(0.0f,0.0f,0.0f,1.f));
+		SettingsManager.SetProjection(1.0,4000.0,70.0);
+		// Load shader
+		m_gbuffer_shader = CShaderManager::Instance().LoadShader("GBuffer.shader");
+		// Load GI
+		m_GI = new DeferredLighting(RootSceneGraph);
+		m_GI->SetFBOGraphicBuffer(m_gbuffer_shader->GetFBO());
+		// Create light 1
+//		PointLight light1;
+//		light1.LightColor = Color(1.0,1.0,1.0,0.0);
+//		light1.Position = Math::TVector3F(0,20,0);
+//		light1.LightRaduis = 100.0;
+//		light1.LightIntensity = 1.0;
+//		m_GI->AddPointLight(light1);
+		// Create light 2
+		SpotLight light2;
+		light2.LightColor = Color(1.0,1.0,1.0,0.0);
+		light2.Position = Math::TVector3F(-500,200,0);
+		light2.LightRaduis = 4000.0;
+		light2.LightIntensity = 1.0;
+		light2.LightCutOff = 70;
+		light2.Direction = Math::TVector3F(1.0,0.0,0.0);
+		m_GI->AddSpotLight(light2);
+		// Load scene
+		SceneGraph::AssimpNode* node1 = SceneGraph::AssimpNode::LoadFromFile("sponza.obj");
+		RootSceneGraph.AddChild(node1);
+	}
+
+	virtual void OnUpdate(double delta)
+	{}
+
+	virtual void OnEvent(SDL_Event& event)
+	{
+		if(event.type == SDL_KEYDOWN)
+		{
+			Math::CMatrix4 matrixTransform;
+			 switch(event.key.keysym.sym)
+			 {
+				 case SDLK_F1:
+					 m_debug = !m_debug;
+					 break;
+				 case SDLK_F2:
+					 m_GI->SetDebugMode(!m_GI->isDebugMode());
+					 break;
+			 }
+		}
+
+	}
+
+	virtual void OnRender()
+	{
+		MatrixManager.SetModeMatrix(MATRIX_3D);
+		m_Camera->GetView();
+
+		Math::CMatrix4 P(1,0,0,0,
+						 0,1,0,0,
+						 0,0,1,1,
+						 0,0,0,0);
+
+		Math::CMatrix4 T(0.5,0,0,0,
+						 0,0.5,0,0,
+						 0,0,1,0,
+						 0.5,0.5,0,1);
+
+		Math::CMatrix4 S(-1,0,0,0,
+						 0,-1,0,0,
+						 0,0,1,0,
+						 0,0,1,1);
+
+		Math::CMatrix4 V = MatrixManager.GetMatrix(VIEW_MATRIX);
+		V.a14 = 0;
+		V.a24 = 0;
+		V.a34 = 0;
+		V.a44 = 1;
+
+		P = T*P*S*V.Inverse();
+
+		MatrixManager.SetProjectionMatrix(P);
+		V.Identity();
+
+
+
+		m_gbuffer_shader->Begin();
+		RootSceneGraph.Draw();
+		m_gbuffer_shader->End();
+
+		if(m_debug)
+			m_gbuffer_shader->GetFBO()->DrawDebug();
+		else
+		{
+			m_Camera->GetView();
+			m_GI->ComputeIllumination();
+		}
+
+		MatrixManager.SetModeMatrix(MATRIX_2D);
+
+		Console.Draw();
+	}
+};
+
+#ifdef WIN32
+#include <windows.h>
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#else
+int main()
+#endif
+{
+	CSettingsManager::Instance().LoadFile("../Donnees/Config.xml");
+	CFontManager::Instance().LoadFont("../Donnees/Fonts/Cheeseburger.ttf", "arial");
+
+	std::cout << "[INFO] Begin ..." << std::endl;
+
+	ApplicationWhite window;
+	window.Run();
+	std::cout << "[INFO] ... end." << std::endl;
+	return 0;
+}
