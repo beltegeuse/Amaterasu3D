c5aa450d2e81da9755f0b3c8b4bb4bdd7a2a8bee
beltegeuse <adrien.gruson@gmail.com>
2011-06-07 00:15:12 -0400

    add quaternion in tests.

---
+++ Code/Tests/Amaterasu3D/Math/test_quaternion.cpp
@@ -0,0 +1,14 @@
+#include <boost/test/unit_test.hpp>
+#include <Math/Quaternion.h>
+#include <test_helper.h>
+
+BOOST_AUTO_TEST_CASE(quaternion_3Dvector)
+{
+	Math::CQuaternion q;
+	q.From3DVector(Math::TVector3F(1,0,0));
+	BOOST_CHECK_EQUAL(q.ToMatrix().Transform(Math::TVector3F(1,0,0),0), Math::TVector3F(1,0,0));
+	q.From3DVector(Math::TVector3F(0,1,0));
+	BOOST_CHECK_EQUAL(q.ToMatrix().Transform(Math::TVector3F(1,0,0),0), Math::TVector3F(0,1,0));
+	q.From3DVector(Math::TVector3F(0,0,1));
+	BOOST_CHECK_EQUAL(q.ToMatrix().Transform(Math::TVector3F(1,0,0),0), Math::TVector3F(0,0,1));
+}
