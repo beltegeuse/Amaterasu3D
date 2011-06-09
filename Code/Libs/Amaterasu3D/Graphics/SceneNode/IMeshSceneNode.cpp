86b64a2fd5a925506a266f0647f4655fb3ef6d67
beltegeuse <adrien.gruson@gmail.com>
2011-06-09 19:13:59 -0400

    Add empty IMeshSceneNode

---
+++ Code/Libs/Amaterasu3D/Graphics/SceneNode/IMeshSceneNode.cpp
@@ -0,0 +1,17 @@
+/*
+ * IMeshSceneNode.cpp
+ *
+ *  Created on: Jun 9, 2011
+ *      Author: adrien
+ */
+
+#include "IMeshSceneNode.h"
+
+IMeshSceneNode::IMeshSceneNode() {
+	// TODO Auto-generated constructor stub
+
+}
+
+IMeshSceneNode::~IMeshSceneNode() {
+	// TODO Auto-generated destructor stub
+}
---
+++ Code/Libs/Amaterasu3D/Graphics/SceneNode/IMeshSceneNode.h
@@ -0,0 +1,20 @@
+/*
+ * IMeshSceneNode.h
+ *
+ *  Created on: Jun 9, 2011
+ *      Author: adrien
+ */
+
+#ifndef IMESHSCENENODE_H_
+#define IMESHSCENENODE_H_
+
+#include <Graphics/SceneNode/ISceneNode.h>
+
+class IMeshSceneNode : public ISceneNode
+{
+public:
+	IMeshSceneNode();
+	virtual ~IMeshSceneNode();
+};
+
+#endif /* IMESHSCENENODE_H_ */
