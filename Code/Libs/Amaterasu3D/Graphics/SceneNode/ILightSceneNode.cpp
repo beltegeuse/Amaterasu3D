7ffa075eaab02b296657ca80e8ed0e6e659c4824
beltegeuse <adrien.gruson@gmail.com>
2011-06-09 19:01:10 -0400

    Add empty header files

---
+++ Code/Libs/Amaterasu3D/Graphics/SceneNode/ILightSceneNode.cpp
@@ -0,0 +1,17 @@
+/*
+ * ILightSceneNode.cpp
+ *
+ *  Created on: Jun 9, 2011
+ *      Author: adrien
+ */
+
+#include "ILightSceneNode.h"
+
+ILightSceneNode::ILightSceneNode() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ILightSceneNode::~ILightSceneNode() {
+	// TODO Auto-generated destructor stub
+}
---
+++ Code/Libs/Amaterasu3D/Graphics/SceneNode/ILightSceneNode.h
@@ -0,0 +1,20 @@
+/*
+ * ILightSceneNode.h
+ *
+ *  Created on: Jun 9, 2011
+ *      Author: adrien
+ */
+
+#ifndef ILIGHTSCENENODE_H_
+#define ILIGHTSCENENODE_H_
+
+#include <Graphics/SceneNode/ISceneNode.h>
+
+class ILightSceneNode : public ISceneNode
+{
+public:
+	ILightSceneNode();
+	virtual ~ILightSceneNode();
+};
+
+#endif /* ILIGHTSCENENODE_H_ */
