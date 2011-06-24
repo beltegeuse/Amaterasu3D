bc0ec558fdb45032037251f6d6d064c6e816efbb
beltegeuse <adrien.gruson@gmail.com>
2011-06-24 15:12:28 -0400

    Move PWN to an idenpendant project : PCM.

--- Code/Libs/OpenGLPWM/WindowImpl.cpp
+++
@@ -1,9 +0,0 @@
-#include "WindowImpl.h"
-
-#include <Win32/WindowImplWin32.h>
-typedef WindowImplWin32 WindowImplType;
-
-WindowImpl* WindowImpl::Create(const WindowMode& mode, const std::string& name, const OpenGLContextSettings& settings)
-{
-	return new WindowImplType(mode, name, settings);
-}
